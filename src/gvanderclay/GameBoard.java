28afb04c5d7260beb68e349e519bbc172ad32cd0
gvanderclay <gvanderclay@yahoo.com>
2015-02-24 13:27:55 -0500

    added into GameBoard: 	createBoard(): method that adds all the SudokuCells to the gameBoard 	insertValue(): method that checks if an insert is valid before inserting 			a value into a cell 	isValidInsert(): private method that checks if a value is valid to be 			inserted into a cell

--- src/gvanderclay/GameBoard.java
+++ src/gvanderclay/GameBoard.java
@@ -1,5 +1,39 @@
 package gvanderclay;
 
 public class GameBoard {
-
+	/**Permanent size of the board*/
+	private final int BOARD_SIZE = 9;
+	
+	/**2D array of SudokuCells that represents the GameBoard*/
+	private SudokuCell[][] gameBoard = new SudokuCell[BOARD_SIZE][BOARD_SIZE];
+	
+	public GameBoard(){
+		createBoard();
+	}
+	
+	private void createBoard(){
+		for (int row = 0; row < BOARD_SIZE; row++){
+			for(int column = 0; column < BOARD_SIZE; column++){
+				gameBoard[row][column] = new SudokuCell(row, column);
+			}
+		}
+	}
+	
+	public void insertValue(SudokuCell SudokuCell, int value){
+		if(!isValidInsert(SudokuCell, value))
+			return;
+		else
+			SudokuCell.setValue(value);
+	}
+	
+	private boolean isValidInsert(SudokuCell SudokuCell, int value){
+		for(int row = 0; row < BOARD_SIZE; row++){
+			for(int column = 0; column < BOARD_SIZE; column++){
+				if(gameBoard[row][column].isRelated(SudokuCell) 
+				&& gameBoard[row][column].getValue() == value)
+					return false;
+			}
+		}
+		return true;
+	}
 }
--- src/gvanderclay/SudokuCell.java
+++ src/gvanderclay/SudokuCell.java
@@ -10,8 +10,8 @@ public class SudokuCell {
 	/**Box that the cell is in*/
 	private int box;
 	
-	/**Array of other cells that this cell is related to*/
-	private SudokuCell[] relatedCells = new SudokuCell[20];
+	/**Value that is inside of the cell*/
+	private int value;
 	
 	public SudokuCell(int row, int column){
 		this.row = row;
@@ -19,6 +19,22 @@ public class SudokuCell {
 		setBox();
 	}
 
+	public boolean isRelated(SudokuCell compare){
+		// this would mean they are the same cell
+		if(this.row == compare.getRow() 
+		&& this.column == compare.getColumn() 
+		&& this.box == compare.getBox()){
+			return false;
+		}
+		if(this.row == compare.getRow())
+			return true;
+		if(this.column == compare.getColumn())
+			return true;
+		if(this.box == compare.getBox())
+			return true;
+		return false;
+	}
+	
 	public int getRow() {
 		return row;
 	}
@@ -34,55 +50,64 @@ public class SudokuCell {
 	public void setColumn(int column) {
 		this.column = column;
 	}
+	
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
 
 	public int getBox(){
 		return box;
-	}
-	
-	public SudokuCell[] getRelatedCells() {
-		return relatedCells;
-	}
-
-	public void setRelatedCells(SudokuCell[] relatedCells) {
-		this.relatedCells = relatedCells;
 	}
 	
 	private void setBox(){
 		if((this.row >= 0 && this.row <= 2) && 
 		   (this.column >= 0 && this.column <= 2)){
 					this.box = 1;
+					return;
 		}
 		if((this.row >= 0 && this.row <= 2) && 
 		   (this.column >= 3 && this.column <= 5)){
 					this.box = 2;
+					return;
 		}
 		if((this.row >= 0 && this.row <= 2) && 
 		   (this.column >= 6 && this.column <= 8)){
 					this.box = 3;
+					return;
 		}
 		if((this.row >= 3 && this.row <= 5) && 
 		   (this.column >= 0 && this.column <= 2)){
 					this.box = 4;
+					return;
 		}
 		if((this.row >= 3 && this.row <= 5) && 
 		   (this.column >= 3 && this.column <= 5)){
 					this.box = 5;
+					return;
 		}
 		if((this.row >= 3 && this.row <= 5) && 
 		   (this.column >= 6 && this.column <= 8)){
 					this.box = 6;
+					return;
 		}
 		if((this.row >= 6 && this.row <= 8) && 
 		   (this.column >= 0 && this.column <= 2)){
 					this.box = 7;
+					return;
 		}
 		if((this.row >= 6 && this.row <= 8) && 
 		   (this.column >= 3 && this.column <= 5)){
 					this.box = 8;
+					return;
 		}
 		if((this.row >= 6 && this.row <= 8) && 
 		   (this.column >= 6 && this.column <= 8)){
 					this.box = 9;
+					return;
 		}
 	}
 }
