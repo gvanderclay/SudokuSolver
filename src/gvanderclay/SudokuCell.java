a903ab010ac52db2ee591a0a08e11c4189e7c70c
gvanderclay <gvanderclay@yahoo.com>
2015-02-24 12:30:33 -0500

    Added the setBox() method to the SudokuCell class. This defines the box that the SudokuCell is in

--- src/gvanderclay/SudokuCell.java
+++ src/gvanderclay/SudokuCell.java
@@ -7,10 +7,82 @@ public class SudokuCell {
 	/**Column that the cell is in*/
 	private int column;
 	
+	/**Box that the cell is in*/
+	private int box;
+	
 	/**Array of other cells that this cell is related to*/
 	private SudokuCell[] relatedCells = new SudokuCell[20];
 	
 	public SudokuCell(int row, int column){
-		row = this.row;
+		this.row = row;
+		this.column = column;
+		setBox();
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	public int getBox(){
+		return box;
+	}
+	
+	public SudokuCell[] getRelatedCells() {
+		return relatedCells;
+	}
+
+	public void setRelatedCells(SudokuCell[] relatedCells) {
+		this.relatedCells = relatedCells;
+	}
+	
+	private void setBox(){
+		if((this.row >= 0 && this.row <= 2) && 
+		   (this.column >= 0 && this.column <= 2)){
+					this.box = 1;
+		}
+		if((this.row >= 0 && this.row <= 2) && 
+		   (this.column >= 3 && this.column <= 5)){
+					this.box = 2;
+		}
+		if((this.row >= 0 && this.row <= 2) && 
+		   (this.column >= 6 && this.column <= 8)){
+					this.box = 3;
+		}
+		if((this.row >= 3 && this.row <= 5) && 
+		   (this.column >= 0 && this.column <= 2)){
+					this.box = 4;
+		}
+		if((this.row >= 3 && this.row <= 5) && 
+		   (this.column >= 3 && this.column <= 5)){
+					this.box = 5;
+		}
+		if((this.row >= 3 && this.row <= 5) && 
+		   (this.column >= 6 && this.column <= 8)){
+					this.box = 6;
+		}
+		if((this.row >= 6 && this.row <= 8) && 
+		   (this.column >= 0 && this.column <= 2)){
+					this.box = 7;
+		}
+		if((this.row >= 6 && this.row <= 8) && 
+		   (this.column >= 3 && this.column <= 5)){
+					this.box = 8;
+		}
+		if((this.row >= 6 && this.row <= 8) && 
+		   (this.column >= 6 && this.column <= 8)){
+					this.box = 9;
+		}
 	}
 }
